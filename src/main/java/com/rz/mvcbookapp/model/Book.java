a1fe99541571f4ee6562ab084838f884fbeaf4c6
rafzoio <raphael.zoio@hotmail.com>
2023-04-06 19:48:49 +0100

    Added lombok dependency

--- src/main/java/com/rz/mvcbookapp/model/Book.java
+++ src/main/java/com/rz/mvcbookapp/model/Book.java
@@ -1,30 +1,21 @@
 package com.rz.mvcbookapp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
 
-	int id;
-	String title;
-	String author;
-	String date;
-	String genres;
-	String characters;
-	String synopsis;
-	
-	
-	public Book(int id, String title, String author, String date, String genres, String characters, String synopsis) {
-
-		this.id = id;
-		this.title = title;
-		this.author = author;
-		this.date = date;
-		this.genres = genres;
-		this.characters = characters;
-		this.synopsis = synopsis;
-		
-	}
-
-
-	public Book() {}
+	private int id;
+	private String title;
+	private String author;
+	private String date;
+	private String genres;
+	private String characters;
+	private String synopsis;
 
 	public Book(String title, String author, String date, String genres, String characters, String synopsis) {
 		this.title = title;
@@ -33,67 +24,5 @@ public class Book {
 		this.genres = genres;
 		this.characters = characters;
 		this.synopsis = synopsis;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getAuthor() {
-		return author;
-	}
-
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-
-	public String getDate() {
-		return date;
-	}
-
-	public void setDate(String date) {
-		this.date = date;
-	}
-
-	public String getGenres() {
-		return genres;
-	}
-
-	public void setGenres(String genres) {
-		this.genres = genres;
-	}
-
-	public String getCharacters() {
-		return characters;
-	}
-
-	public void setCharacters(String characters) {
-		this.characters = characters;
-	}
-
-	public String getSynopsis() {
-		return synopsis;
-	}
-
-	public void setSynopsis(String synopsis) {
-		this.synopsis = synopsis;
-	}
-
-	@Override
-	public String toString() {
-		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", date=" + date + ", genres=" + genres
-				+ ", synopsis=" + synopsis + ", characters=" + characters + "]";
 	}
 }
